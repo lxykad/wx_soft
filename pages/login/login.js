1b4bfb4ca133e63ffc9fd30fe48ed05c2378eb9c
lxykad <503215875@qq.com>
2016-10-31 12:13:34 +0800

    add input

--- pages/login/login.js
+++ pages/login/login.js
@@ -1,6 +1,8 @@
 
 Page({
   data:{
+
+    name:null
    
   },
   onLoad:function(options){
@@ -22,5 +24,25 @@ Page({
   onUnload:function(){
     // 页面关闭
     
+  },
+  nameInput:function(event){
+    //console.log(event.detail.value);
+    
+    this.setData({name:event.detail.value});
+    console.log(name);
+  },
+
+  clickLogin : function(){
+
+    //  wx.showToast({
+    //         title: name,
+    //         icon: 'success',
+    //         duration: 2000
+    //     })
+    //console.log("login====name==="+name+"=====password===="+222);
   }
+
+
+
+
 })
